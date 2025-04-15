88dca36c5e20d1101f3890118bf8b380e04a944b
Dennis Wachira <wachiradenis336@gmail.com>
2025-04-15 12:28:14 +0300

    complete lab

---
+++ src/__tests__/About.test.js
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import About from "../components/About";
+import user from "../data/user";
+
+test("renders a <p> element with the bio from props", () => {
+  render(<About bio="I made this" links={user.links} />);
+  expect(screen.queryByText("I made this")).toBeInTheDocument();
+});
+
+test("does not render a <p> element if the bio is not included in props", () => {
+  const { container } = render(<About links={user.links} />);
+  expect(container.querySelector("p")).toBeNull();
+});
+
+test("does not render a <p> element if the bio is an empty string", () => {
+  const { container } = render(<About bio="" links={user.links} />);
+  expect(container.querySelector("p")).toBeNull();
+});
